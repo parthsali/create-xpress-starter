35bacde5ec856b1ab7a270681691cca10a4463cd
Parth Sali <parthsali04@gmail.com>
2024-05-20 19:59:54 +0530

    Add Typescript MVC

---
+++ template/typescript/mvc/README.md
@@ -0,0 +1,1 @@
+### Production-ready Node.js boilerplate with Express, Mongoose (MongoDB), ESLint (TypeScript), and error handling.
--- template/typescript/mvc/src/app.ts
+++ template/typescript/mvc/src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import { Request, Response, NextFunction } from "express";
-import userRouter from "./api/User/userRouter";
 
 const app = express();
 
@@ -12,9 +11,6 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
     .json({ message: "Express Server built using create-xpress-starter" });
 });
 
-app.use("/api/users", userRouter);
-
-// Global Error Handler
 app.use(globalErrorHandler);
 
 export default app;
