5243133e9213256a25e44d6df18dd6dad9708251
ParthSali <parthsali04@gmail.com>
2024-09-01 10:46:30 +0530

    Update JS MA Template

--- template/javascript/mvc/src/middlewares/globalErrorHandler.js
+++ template/javascript/mvc/src/middlewares/globalErrorHandler.js
@@ -1,12 +1,12 @@
 import { config } from "../config/config.js";
 
 const globalErrorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
+  const statusCode = err.statusCode || 500;
 
-    return res.status(statusCode).json({
-        message: err.message,
-        errorStack: config.NODE_ENV === "development" ? err.stack : "",
-    });
+  return res.status(statusCode).json({
+    message: err.message,
+    errorStack: config.NODE_ENV === "development" ? err.stack : "",
+  });
 };
 
 export default globalErrorHandler;
